41304ba67b1d77f96b90a4d5eb322f92a8cef4eb
NickolayNanov <40512673+NickolayNanov@users.noreply.github.com>
2018-10-15 07:43:59 +0300

    Add files via upload

---
+++ Tech-Module/AMinerTask/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AMinerTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> resourses = new Dictionary<string, int>();
+
+            string input;
+          
+            while((input = Console.ReadLine()) != "stop")
+            {
+                string resourse = input;
+                int quantity = int.Parse(Console.ReadLine());
+
+                if (!resourses.ContainsKey(resourse))
+                {
+                    resourses.Add(resourse, quantity);
+                }
+                else
+                {
+                    resourses[resourse] += quantity;
+                }
+            }
+
+            foreach (var resourse in resourses)
+            {
+                Console.WriteLine($"{resourse.Key} -> {resourse.Value}");
+            }
+        }
+    }
+}
