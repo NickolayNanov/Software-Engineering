126eec1a52d58a366be1bf9a93d3705b2d558f83
NickolayNanov <40512673+NickolayNanov@users.noreply.github.com>
2018-10-18 08:26:37 +0300

    Add files via upload

---
+++ C# Fundamentals/C# Advanced/Dictionaries and HashSets/Dicts And HashSets/Even Times/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Even_Times
+{
+    class Program
+    {
+        static Dictionary<int, int> counter = new Dictionary<int, int>();
+
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                int num = int.Parse(Console.ReadLine());
+
+                if (!counter.ContainsKey(num))
+                {
+                    counter.Add(num, 1);
+                }
+                else
+                {   
+                    counter[num]++;
+                }
+            }
+
+            foreach (var num in counter)
+            {
+                if(num.Value % 2 == 0)
+                {
+                    Console.WriteLine(num.Key);
+                }
+            }
+        }
+    }
+}
